538f14409dbdb9c671235bf630c91f915581516a
Ben Kelly <benkelly@Bens-Mac.local>
2018-08-17 20:44:09 -0400

    JS is functional

--- scripts/app.js
+++ scripts/app.js
@@ -1,62 +1,32 @@
 const dead = {};
 
-dead.questions = [
-    question1 = {
-        title: `Question 1`,
-        image: ``
-    },
-    question2 = {
-        title: `Question 2`,
-        image: ``
-    },
-    question3 = {
-        title: `Question 3`,
-        image: ``
-    },
-    question4 = {
-        title: `Question 4`,
-        image: ``
-    },
-    question5 = {
-        title: `Question 5`,
-        image: ``
-    }
-];
-
 dead.score = {
+    total: 0,
     care: 0,
     dontCare: 0
 };
 
 dead.result = {
-        level0: `<h2>level 0</h2>`,
-        level1: `<h2>level 1</h2>`,
-        level2: `<h2>level 2</h2>`,
-        level3: `<h2>level 3</h2>`,
-        level4: `<h2>level 4</h2>`,
-        level5: `<h2>level 5</h2>`
+        level0: `level 0`,
+        level1: `level 1`,
+        level2: `level 2`,
+        level3: `level 3`,
+        level4: `level 4`,
+        level5: `level 5`
 };
-
-dead.start = ()=>{
-    $('.start-button').on('submit', (e)=>{
-        e.preventDefault();
-
-    });
-};
-// listen for submit of each question and update the score (care / dontCare)
 
 dead.next = ()=>{
-    $('form').on('submit', (e) => {
+    $('form').on('submit', function(e){
         e.preventDefault();
         const answer = $('form').find('input:radio:checked').attr('data-answer');
+        const total = $('form').find('input:radio:checked').attr('data-count');
+        $('form').trigger('reset');
         dead.score[answer]++;
+        dead.score[total]++;
+        $(this).parent().parent().addClass('hide');
         console.log(dead.score);
     });
 };
-
-// change slide after submit
-// compare array lengths to each other to calculate score of dead inside (convert to percentage)
-// listen for submit on result button and then populate the answer in final slide
 
 dead.init = ()=>{
     dead.next();
